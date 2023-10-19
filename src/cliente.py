f4cf7ffd8388f1d5cfa59643f81a3a29da5cda7a
jacasho22 <37211613+jacasho22@users.noreply.github.com>
2023-10-20 01:23:47 +0200

    Create cliente.py

---
+++ src/cliente.py
@@ -0,0 +1,8 @@
+# cliente.py
+class Cliente:
+    def __init__(self, nombre):
+        self.nombre = nombre
+    def realizar_pedido(self, medicamento, direccion_entrega):
+        pass
+if __name__ == "__main__":
+    cliente1 = Cliente("ClienteX")
